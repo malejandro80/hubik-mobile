546ce132e8552513e2ffd024c208f323b4c2331d
Miguel Bastidas <m.alejandro_80@hotmail.com>
2026-09-15 13:32:19 -0500

    chore: add langgraph, genai deps and arch-team script

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   watchman: false,
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|@langchain|@google)',
   ],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
